9688ae7f600bcdf1d33c7e0729aef6e2e5060809
hrdy <sonushashankmot@gmail.com>
2021-05-23 16:57:58 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express=require('express')
 const hbs=require('hbs')
 const forecast = require('./utils/forecast')
 const geocode=require('./utils/geocode')
-const { getMaxListeners } = require('process')
+
 
 const app=express()
 const port=process.env.PORT || 3000
@@ -88,6 +88,6 @@ app.get('*',(req,res)=>{
 
 })
 app.listen(port,()=>{
-    console.log('Server is up on port port '+port)
+    console.log('Server is up on port '+port)
 })
 
