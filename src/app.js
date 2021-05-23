9d6d91ddda5a1cd7be91cf2a9044f112cd87bf4d
hrdy <sonushashankmot@gmail.com>
2021-05-23 18:27:58 +0530

    success

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
     res.render('index',{
         title:'WEATHA',
-        name:' Sonu Shashank'
+        name:'Sonu Shashank'
     })
 })
 app.get('/about',(req,res)=>{
